b29244abc3f64ec6bb0942c6d7654f2ebdf18aa7
ddonche <dan@fatalfitness.com>
2020-11-12 21:50:30 -0700

    working on note detail screen

---
+++ lib/screens/note_detail_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NoteDetailScreen extends StatefulWidget {
+  @override
+  _NoteDetailScreenState createState() => _NoteDetailScreenState();
+}
+
+class _NoteDetailScreenState extends State<NoteDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
