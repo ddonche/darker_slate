c16b2c7f33d520816a608419f048ccbf9a552067
ddonche <dan@fatalfitness.com>
2020-11-25 19:31:08 -0700

    FB and gmail images, leaderboard page

---
+++ lib/screens/leaderboard_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LeaderboardScreen extends StatelessWidget {
+  static const String id = 'leaderboard_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
