1a33a89c038fa15310a323be8ddae63fd6605978
ddonche <dan@fatalfitness.com>
2020-12-03 20:13:07 -0700

    tidying up

---
+++ lib/screens/account_settings_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AccountSettingsScreen extends StatefulWidget {
+  static const String id = 'account_settings_screen';
+  @override
+  _AccountSettingsScreenState createState() => _AccountSettingsScreenState();
+}
+
+class _AccountSettingsScreenState extends State<AccountSettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
